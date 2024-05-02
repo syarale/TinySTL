520da4f6d5d45afb31fdec36049e9087ab7b133c
syarale <syinlei2023@gmail.com>
2024-05-02 21:45:08 +0800

    Add allocator: malloc_alloc and test
    Signed-off-by: syarale <syinlei2023@gmail.com>

--- allocator/alloc.h
+++ allocator/alloc.h
@@ -2,16 +2,79 @@
 #define ALLOCATOR_ALLOC_H_
 
 #include <cstddef>
+#include <cstdlib>
+#include <new>
 
 namespace sgi {
 
 // first level memory allocator
 class malloc_alloc {
- public:
-  static void* allocate(std::size_t n) { return ::operator new(n); }
+  using Func = void (*)();
 
-  static void deallocate(void* p, std::size_t n) { ::operator delete(p, n); }
+ public:
+  static void* allocate(std::size_t n) {
+    void* ptr = malloc(n);
+    if (ptr == NULL) {
+      ptr = oom_malloc(n);
+    }
+    return ptr;
+  }
+
+  static void deallocate(void* p, std::size_t n) { std::free(p); }
+
+  static void* reallocate(void* p, std::size_t new_sz) {
+    void* ptr = realloc(p, new_sz);
+    if (ptr == NULL) {
+      ptr = oom_realloc(p, new_sz);
+    }
+    return ptr;
+  }
+
+  static Func set_new_handler(Func func) {
+    Func old_handler = oom_handler;
+    oom_handler = func;
+    return (old_handler);
+  }
+
+ private:
+  static void* oom_malloc(std::size_t n);
+  static void* oom_realloc(void* p, std::size_t n);
+  static void (*oom_handler)();
 };
+
+void (*malloc_alloc::oom_handler)() = nullptr;
+
+void* malloc_alloc::oom_malloc(std::size_t n) {
+  void* res = NULL;
+  for (;;) {
+    if (oom_handler == NULL) {
+      throw std::bad_alloc();
+      exit(1);
+    }
+
+    (*oom_handler)();
+    res = std::malloc(n);
+    if (res != NULL) {
+      return res;
+    }
+  }
+}
+
+void* malloc_alloc::oom_realloc(void* p, std::size_t n) {
+  void* res = NULL;
+  for (;;) {
+    if (oom_handler == NULL) {
+      throw std::bad_alloc();
+      exit(1);
+    }
+
+    (*oom_handler)();
+    res = std::realloc(p, n);
+    if (res != NULL) {
+      return res;
+    }
+  }
+}
 
 // second level memory allocator
 class default_alloc {
--- allocator/alloc_test.cc
+++ allocator/alloc_test.cc
@@ -1,12 +1,26 @@
 #include "alloc.h"
 
+#include "construct.h"
 #include "gtest/gtest.h"
 
 constexpr int NUM = 10;
+constexpr int INIT_VALUE = 101;
 
-TEST(Alloc, allocator) {
-  sgi::allocator<int, sgi::alloc> alloc;
-  int* p = alloc.allocate(NUM);
+class Foo {
+ public:
+  int value = INIT_VALUE;
+};
+
+TEST(Alloc, malloc_alloc) {
+  sgi::allocator<Foo, sgi::malloc_alloc> alloc;
+  Foo* p = alloc.allocate(NUM);
+  EXPECT_NE(p, nullptr);
+  for (int i = 0; i < NUM; i++) {
+    sgi::construct(p + i);
+    EXPECT_EQ((p + i)->value, INIT_VALUE);
+  }
+
+  sgi::destroy(p, p + NUM);
   alloc.deallocate(p, NUM);
 }
 
