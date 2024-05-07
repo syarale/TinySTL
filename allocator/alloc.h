29673ea649185ed66b455573d027417af628acf8
leisy <syinlei2023@gmail.com>
2024-05-07 18:10:45 +0800

    WIP: implement default_alloc
    Signed-off-by: leisy <syinlei2023@gmail.com>

--- allocator/alloc.h
+++ allocator/alloc.h
@@ -1,18 +1,27 @@
 #ifndef ALLOCATOR_ALLOC_H_
 #define ALLOCATOR_ALLOC_H_
 
+#include <cassert>
 #include <cstddef>
 #include <cstdlib>
+#include <cstring>
 #include <new>
 
 namespace sgi {
+
+using size_t = std::size_t;
+
+inline constexpr int ALIGN = 8;
+inline constexpr int MAX_BYTES = 128;
+inline constexpr int NFREELISTS = MAX_BYTES / ALIGN;
+inline constexpr int DEFAULT_CHUNKS = 20;
 
 // first level memory allocator
 class malloc_alloc {
   using Func = void (*)();
 
  public:
-  static void* allocate(std::size_t n) {
+  static void* allocate(size_t n) {
     void* ptr = malloc(n);
     if (ptr == NULL) {
       ptr = oom_malloc(n);
@@ -20,9 +29,9 @@ class malloc_alloc {
     return ptr;
   }
 
-  static void deallocate(void* p, std::size_t n) { std::free(p); }
+  static void deallocate(void* p, size_t n) { std::free(p); }
 
-  static void* reallocate(void* p, std::size_t new_sz) {
+  static void* reallocate(void* p, size_t new_sz) {
     void* ptr = realloc(p, new_sz);
     if (ptr == NULL) {
       ptr = oom_realloc(p, new_sz);
@@ -37,14 +46,14 @@ class malloc_alloc {
   }
 
  private:
-  static void* oom_malloc(std::size_t n);
-  static void* oom_realloc(void* p, std::size_t n);
+  static void* oom_malloc(size_t n);
+  static void* oom_realloc(void* p, size_t n);
   static void (*oom_handler)();
 };
 
 void (*malloc_alloc::oom_handler)() = nullptr;
 
-void* malloc_alloc::oom_malloc(std::size_t n) {
+void* malloc_alloc::oom_malloc(size_t n) {
   void* res = NULL;
   for (;;) {
     if (oom_handler == NULL) {
@@ -60,7 +69,7 @@ void* malloc_alloc::oom_malloc(std::size_t n) {
   }
 }
 
-void* malloc_alloc::oom_realloc(void* p, std::size_t n) {
+void* malloc_alloc::oom_realloc(void* p, size_t n) {
   void* res = NULL;
   for (;;) {
     if (oom_handler == NULL) {
@@ -79,10 +88,109 @@ void* malloc_alloc::oom_realloc(void* p, std::size_t n) {
 // second level memory allocator
 class default_alloc {
  public:
-  static void* allocate(std::size_t n) { return ::operator new(n); }
+  static void* allocate(size_t bytes) {
+    assert(bytes > 0);
 
-  static void deallocate(void* p, std::size_t n) { ::operator delete(p, n); }
+    // use malloc_alloc to allocate larger memory
+    if (bytes > MAX_BYTES) {
+      return malloc_alloc::allocate(bytes);
+    }
+
+    bytes = round_up(bytes);
+    size_t index = free_lists_index(bytes);
+    obj* free_head = free_lists[index];
+    if (free_head == nullptr) {
+      void* p = refill(bytes);
+      return p;
+    }
+
+    free_lists[index] = free_head->next_free_obj;
+    free_head->next_free_obj = nullptr;
+    return free_head;
+  }
+
+  static void deallocate(void* p, size_t bytes) {
+    if (bytes > MAX_BYTES) {
+      return malloc_alloc::deallocate(p, bytes);
+    }
+
+    bytes = round_up(bytes);
+    size_t index = free_lists_index(bytes);
+
+    obj* free_head = free_lists[index];
+    static_cast<obj*>(p)->next_free_obj = free_head;
+    free_lists[index] = static_cast<obj*>(p);
+  }
+
+  static void* reallocate(void* p, size_t old_sz, size_t new_sz) {
+    if (old_sz > MAX_BYTES && new_sz > MAX_BYTES) {
+      return std::realloc(p, new_sz);
+    }
+
+    if (round_up(old_sz) == round_up(new_sz)) {
+      return p;
+    }
+
+    void* new_p = allocate(new_sz);
+    size_t copy_size = old_sz < new_sz ? old_sz : new_sz;
+    memmove(new_p, p, copy_size);
+    deallocate(p, old_sz);
+    return new_p;
+  }
+
+ private:
+  union obj {
+    obj* next_free_obj;
+    char data[1];
+  };
+
+  static size_t round_up(size_t bytes) {
+    return (bytes + ALIGN - 1) & (~(ALIGN - 1));
+  }
+
+  static size_t free_lists_index(size_t bytes) {
+    return (bytes + ALIGN - 1) / ALIGN - 1;
+  }
+
+  static char* alloc_chunk(size_t bytes, int& objs);  // nobj may change
+  static char* refill(size_t bytes);
+
+  static obj* free_lists[NFREELISTS];
+
+  // memory pool
+  static char* start_free;
+  static char* end_free;
+  static size_t extend_heap_size;
 };
+
+default_alloc::obj* default_alloc::free_lists[NFREELISTS] = {nullptr};
+char* default_alloc::start_free = nullptr;
+char* default_alloc::end_free = nullptr;
+size_t default_alloc::extend_heap_size = 0;
+
+char* default_alloc::alloc_chunk(size_t bytes, int& nobjs) {}
+
+char* default_alloc::refill(size_t bytes) {
+  int nobjs = DEFAULT_CHUNKS;
+  char* start = alloc_chunk(bytes, nobjs);  // nobjs may change
+
+  if (nobjs == 1) {
+    return start;
+  }
+
+  obj* prev = reinterpret_cast<obj*>(start);
+  prev->next_free_obj = nullptr;
+  for (int i = 1; i < nobjs; i++) {
+    obj* curr = reinterpret_cast<obj*>(start + i * bytes);
+    curr->next_free_obj = nullptr;
+    prev->next_free_obj = curr;
+    prev = curr;
+  }
+
+  int index = free_lists_index(bytes);
+  free_lists[index] = reinterpret_cast<obj*>(start)->next_free_obj;
+  return start;
+}
 
 // In fact, __USE_MALLOC_ALLOC macro is never defined in SGI-STL
 #ifdef __USE_MALLOC_ALLOC
@@ -94,15 +202,15 @@ using alloc = default_alloc;
 template <typename T, typename Alloc>
 class allocator {
  public:
-  static T* allocate(std::size_t n) {
-    return n == 0 ? nullptr : static_cast<T*>(Alloc::allocate(n * sizeof(T)));
+  static T* allocate(size_t n) {
+    return n <= 0 ? nullptr : static_cast<T*>(Alloc::allocate(n * sizeof(T)));
   }
 
   static T* allocate(void) {
     return static_cast<T*>(Alloc::allocate(sizeof(T)));
   }
 
-  static void deallocate(T* p, std::size_t n) {
+  static void deallocate(T* p, size_t n) {
     if (n != 0) {
       Alloc::deallocate(p, n * sizeof(T));
     }
--- allocator/alloc_test.cc
+++ allocator/alloc_test.cc
@@ -3,6 +3,8 @@
 #include "construct.h"
 #include "gtest/gtest.h"
 
+#define SIKP_PRIVATE_MEMBER_CASE 0
+
 constexpr int NUM = 10;
 constexpr int INIT_VALUE = 101;
 
@@ -11,7 +13,7 @@ class Foo {
   int value = INIT_VALUE;
 };
 
-TEST(Alloc, malloc_alloc) {
+TEST(MallocAlloc, malloc_alloc) {
   sgi::allocator<Foo, sgi::malloc_alloc> alloc;
   Foo* p = alloc.allocate(NUM);
   EXPECT_NE(p, nullptr);
@@ -23,6 +25,34 @@ TEST(Alloc, malloc_alloc) {
   sgi::destroy(p, p + NUM);
   alloc.deallocate(p, NUM);
 }
+
+#ifndef SIKP_PRIVATE_MEMBER_CASE
+TEST(DefaultAlloc, round_up) {
+  // Before running this test case, round_up should be temporarily
+  // modified to a public attribute
+
+  using alloc = sgi::default_alloc;
+  EXPECT_EQ(alloc::round_up(128), 128);
+  EXPECT_EQ(alloc::round_up(110), 112);
+  EXPECT_EQ(alloc::round_up(90), 96);
+  EXPECT_EQ(alloc::round_up(88), 88);
+  EXPECT_EQ(alloc::round_up(56), 56);
+  EXPECT_EQ(alloc::round_up(2), 8);
+  EXPECT_EQ(alloc::round_up(0), 0);
+}
+
+TEST(DefaultAlloc, free_lists_index) {
+  // Before running this test case, free_lists_index should be temporarily
+  // modified to a public attribute
+  using alloc = sgi::default_alloc;
+  EXPECT_EQ(alloc::free_lists_index(128), 15);
+  EXPECT_EQ(alloc::free_lists_index(120), 14);
+  EXPECT_EQ(alloc::free_lists_index(88), 10);
+  EXPECT_EQ(alloc::free_lists_index(56), 6);
+  EXPECT_EQ(alloc::free_lists_index(16), 1);
+  EXPECT_EQ(alloc::free_lists_index(8), 0);
+}
+#endif
 
 int main(int argc, char** argv) {
   ::testing::InitGoogleTest(&argc, argv);
