540d0afa514057baf9913627c04216ac67d833b2
syarale <syinlei2023@gmail.com>
2024-05-15 21:42:47 +0800

    WIP: feature(vector): define APIs
    Signed-off-by: syarale <syinlei2023@gmail.com>

--- vector/vector.h
+++ vector/vector.h
@@ -1,4 +1,50 @@
 #ifndef VECTOR_VECTOR_H_
 #define VECTOR_VECTOR_H_
 
+#include "alloc.h"
+
+template <typename T, typename Alloc = sgi::alloc>
+class vector {
+ public:
+  using value_type = T;
+  using pointer = value_type*;
+  using reference = value_type&;
+  using iterator = value_type*;
+  using size_type = std::size_t;
+  using difference_type = std::ptrdiff_t;
+
+  vector() = default;
+  vector(size_type n, const T& value) {}
+  explicit vector(size_type n) {}
+
+  ~vector() = default;
+
+  void begin() {}
+  void end() {}
+  void size() {}
+  void capacity() {}
+  void empty() {}
+  reference operator[](size_type n) {}
+
+  reference front() {}
+  reference back() {}
+  void push_back() {}
+  void pop_back() {}
+
+  void insert(iterator position, const T& value) {}
+  void insert(iterator position, size_type n, const T& value) {}
+  iterator erase(iterator position) {}
+  iterator erase(iterator first, iterator last) {}
+  void resize(size_type n) {}
+  void resize(size_type n, const T& value) {}
+
+  void clear() {}
+
+ private:
+  using data_allocator = sgi::allocator<value_type, Alloc>;
+  iterator start_;
+  iterator finish_;
+  iterator end_of_storage_;
+};
+
 #endif  // VECTOR_VECTOR_H_
